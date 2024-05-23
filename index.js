de811e95b3cb217e420146b2a649076b1d33cab8
Hydra <github@equipehydra.com>
2024-05-23 04:19:24 -0300

    Versão 1

---
+++ index.js
@@ -0,0 +1,5 @@
+// JavaScript Document
+
+// index.js
+import '@material/web/textfield/outlined-text-field.js;
+import '@material/web/icon/icon.js';
---
+++ scripts.js
@@ -0,0 +1,78 @@
+// JavaScript Document
+
+// Selecionando o elemento de input do CNPJ
+const inputCNPJ = document.getElementById('input-cnpj');
+
+// Adicionando um evento de input ao campo de entrada de CNPJ
+inputCNPJ.addEventListener('input', function() {
+    const cnpjValue = inputCNPJ.value.replace(/\D/g, ''); // Remover caracteres não numéricos
+
+    // Configuração para CNPJ
+    inputCNPJ.maxLength = 18;
+
+    // Formatação para CNPJ (##.###.###/####-##)
+    let formattedCNPJ = cnpjValue.substring(0, 2);
+    if (cnpjValue.length > 2) formattedCNPJ += '.' + cnpjValue.substring(2, 5);
+    if (cnpjValue.length > 5) formattedCNPJ += '.' + cnpjValue.substring(5, 8);
+    if (cnpjValue.length > 8) formattedCNPJ += '/' + cnpjValue.substring(8, 12);
+    if (cnpjValue.length > 12) formattedCNPJ += '-' + cnpjValue.substring(12, 14);
+
+    inputCNPJ.value = formattedCNPJ;
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+        const statusSelect = document.getElementById('status');
+        const inputStatus = document.getElementById('input-status');
+
+        // Define o valor padrão do campo de entrada para "12x"
+        inputStatus.value = "Concluída";
+        inputStatus.readOnly = true;
+
+        // Adiciona um ouvinte de evento para monitorar a mudança na seleção
+        statusSelect.addEventListener('change', function() {
+            const selectedOption = statusSelect.value; // Obtém o valor da opção selecionada
+
+            if (selectedOption === 'Outro') {
+                // Se "Outro" for selecionado, permite a edição e limpa o valor e o placeholder
+                inputStatus.readOnly = false;
+                inputStatus.value = "";
+                inputStatus.placeholder = "Insira aqui o STATUS";
+            } else {
+                // Se outra opção for selecionada, atualiza o valor do campo de entrada e o torna somente leitura
+                inputStatus.value = selectedOption;
+                inputStatus.readOnly = true;
+                inputStatus.placeholder = ""; // Remove o placeholder se não for "Outro"
+                }
+            });
+        });
+
+document.addEventListener('DOMContentLoaded', function() {
+        const pixSelect = document.getElementById('pix');
+        const inputPix = document.getElementById('input-pix');
+
+        // Define o valor padrão do campo de entrada para "12x"
+        inputPix.value = "Não utilizada";
+        inputPix.readOnly = true;
+
+        // Adiciona um ouvinte de evento para monitorar a mudança na seleção
+        pixSelect.addEventListener('change', function() {
+            const selectedOption = pixSelect.value; // Obtém o valor da opção selecionada
+
+            if (selectedOption === 'Outro') {
+                // Se "Outro" for selecionado, permite a edição e limpa o valor e o placeholder
+                inputPix.readOnly = false;
+                inputPix.value = "";
+                inputPix.placeholder = "Insira aqui a CHAVE PIX";
+            } else {
+                // Se outra opção for selecionada, atualiza o valor do campo de entrada e o torna somente leitura
+                inputPix.value = selectedOption;
+                inputPix.readOnly = true;
+                inputPix.placeholder = ""; // Remove o placeholder se não for "Outro"
+                }
+            });
+        });
+
+document.getElementById('input-cnpj').addEventListener('input', function() {
+            var cnpj = this.value;
+            document.title = "Banco BS2 - Comprovante [" + cnpj + "]";
+        });
